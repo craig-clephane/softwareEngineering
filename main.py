2fdb1a941c2c3df2d8b444b7947e6a4998cf6fc0
will <13015928+willwoodruff@users.noreply.github.com>
2019-10-23 13:18:56 +0100

    Initialising Python Project

---
+++ main.py
@@ -0,0 +1,3 @@
+# Using Python 3 syntax as version 2 is being deprecated at the beginning of 2020
+print('Hello, World!')
+print('This is the Spotify integrated, News Application where we find yo songs based on the days top stories')
