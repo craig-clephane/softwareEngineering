4f8234253eacebaacd37041602143d31512e768a
craig-clephane <34477748+craig-clephane@users.noreply.github.com>
2019-11-04 22:43:45 +0000

    Added functions and stored Headlines into an array

--- news_init.py
+++ news_init.py
@@ -1,21 +1,38 @@
 #Created 23/10/2019
-#Last Edited: 23/10/2019
+#Last Edited: 04/11/2019
 #Author(s): Craig Clephane
 #Collaborator(s): -
 
 from newsapi import NewsApiClient
 import requests
 
-## Contains API key for project 
-newsapi = NewsApiClient(api_key='96113538df2a451caefde995afc26143')
+articleTitles = []
 
-## Makes a HTTP Request for content within the US
-url = ('https://newsapi.org/v2/top-headlines?'
-       'country=us&'
-       'apiKey=96113538df2a451caefde995afc26143')
+def accessNewsAPI():
+       ## Contains API key for project 
+       # newsapi = NewsApiClient(api_key='96113538df2a451caefde995afc26143')
+
+       ## Makes a HTTP Request for content within the US
+       url = ('https://newsapi.org/v2/top-headlines?'
+              'country=gb&'
+              'apiKey=96113538df2a451caefde995afc26143')
        
-## Stores content inside variable 
-response = requests.get(url)
+       ## Stores content inside variable 
+       response = requests.get(url).json()
+       printsContent(response)
 
-## Prints Latest news in the form for a json string 
-print (response.json())
+def printsContent(response): 
+       i = 0
+       while i < 20:
+              print (response['articles'][i]['title'])
+              storeArray(response['articles'][i]['title'])
+              i = i+1
+
+def storeArray(response):
+       articleTitles.append(response)
+       
+def returnTitles():
+       return articleTitles
+
+
+accessNewsAPI()
