61d2342bdd8bdf61b06a23468cf4074df5134bed
craig-clephane <34477748+craig-clephane@users.noreply.github.com>
2019-11-07 17:57:35 +0000

    Added RAKE to extract phrases WIP

--- news_init.py
+++ news_init.py
@@ -1,10 +1,13 @@
 #Created 23/10/2019
-#Last Edited: 04/11/2019
+#Last Edited: 07/11/2019
 #Author(s): Craig Clephane
 #Collaborator(s): -
 
-from newsapi import NewsApiClient
+from newsapi.newsapi_client import NewsApiClient
 import requests
+stop_dir = "SmartStoplist.txt"
+from rake_nltk import rake
+import operator
 
 articleTitles = []
 
@@ -12,7 +15,7 @@ def accessNewsAPI():
        ## Contains API key for project 
        # newsapi = NewsApiClient(api_key='96113538df2a451caefde995afc26143')
 
-       ## Makes a HTTP Request for content within the US
+       ## Makes a HTTP Request for content within gb
        url = ('https://newsapi.org/v2/top-headlines?'
               'country=gb&'
               'apiKey=96113538df2a451caefde995afc26143')
@@ -30,9 +33,20 @@ def printsContent(response):
 
 def storeArray(response):
        articleTitles.append(response)
+       phrases(response)
        
 def returnTitles():
        return articleTitles
 
+def phrases(response):
+       rake_object = rake.Rake(stop_dir)
+       rake_object.extract_keywords_from_text(response)
+       print(rake_object.get_ranked_phrases()) 
 
 accessNewsAPI()
+
+
+
+
+
+
