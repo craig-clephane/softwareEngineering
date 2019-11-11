fe6eab1aeef6e97974aa6fd2d93d9dabd593f05b
craig-clephane <34477748+craig-clephane@users.noreply.github.com>
2019-11-11 14:01:15 +0000

    Call News and twitter API

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 # Using Python 3 syntax as version 2 is being deprecated at the beginning of 2020
-print('Hello, World!')
-print('This is the Spotify integrated, News Application where we find you songs based on the days top stories')
+import news_init
+import spotify_init
+
+newsHeadlines = news_init.accessNewsAPI()
+
+for row in newsHeadlines:
+    for i in row:
+        spotify_init.runRequest(i)
+    
