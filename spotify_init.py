709ade52c5ced6d4d25d9875767b7f531bb696a5
craig-clephane <34477748+craig-clephane@users.noreply.github.com>
2019-11-13 22:58:52 +0000

    Updated to add tracks to playlist

--- spotify_init.py
+++ spotify_init.py
@@ -9,7 +9,7 @@ import json
 headers = {
     #'Accept': 'application/json',
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer BQA7KQzRzrqzEGDLSYR2nMdQR_MKMxnCSJVS80M9lxy5nNofgm8FASq5QQilk_fl3Mx3wWYRnYkjbNsI-1AJe0BNVUWy0VlJVSHu6OBqi-zWVc4KOdfhyRF_sJPwbvQQMO54he5aWULguPthJtbvO28MO7U0HPpQksPaUUm9olXOUZuwul_irqd3Y2m5Hyg',
+    'Authorization': 'Bearer BQBz0dl-5IUhuX9GBiqSkY4PBw_feRjYox3s8uimXPwkUbrtifLmAy1yIyZGI3bBKxI32OZxjASxizegJdGcF38XLx9fJ_GvIGXoXjKqOsJyMv5mNV6kbTjxJvqb7x9eMA0GIg3BPXRjWDTuIXZr9_hTcUlAv6l1johypMxaPGI5D8vi1FmWhbqBU9cPr4C9RBbQheQpg_bzBSJI3Gy9GiZbaPz7miHRLHU56iLS-jhp7Nqw2RXygd1hxdaaWjYXOorqqbLWfm3TljYAvPjp_g4voP1aAA',
 }
 
 
@@ -22,18 +22,26 @@ def runRequest(value):
 
     response = requests.get('https://api.spotify.com/v1/search', headers=headers, params=params)
     json_data = json.loads(response.text)
-
+   
     if response.status_code == 200:
         print('Success! We are generating your songs!')
-        #print(json_data)
+        print(json_data)
+
     elif response.status_code == 404:
         print('Not Found. Sorry, there wern\'t any results for that article')
     else:
         print('An error occurred -', response.status_code)
+    
+    print(len(json_data["tracks"]["items"]))
+    if len(json_data["tracks"]["items"]) is not 0:
+        song = json_data["tracks"]["items"][0]["id"]
+        print(song)
+        return song
+    
+    
 
-    song = json_data["tracks"]["items"][0]["album"]['id']
-    #print(song)
-    return song
+
+    
 
 
 def createplaylist():
@@ -44,13 +52,8 @@ def createplaylist():
 
 def addtrack(track):
     params = (
-    ('uris', 'spotify:track:',track )
-)
+        ('uris', 'spotify:track:'+track),
+    )
 
-    response = requests.post('https://api.spotify.com/v1/playlists/7aEjvDQ1eCifCjOrHoqQNi/tracks', headers=headers, params=params)
+    response = requests.post('https://api.spotify.com/v1/playlists/1HsXvdPOcUvIsamfEMxuaM/tracks', headers=headers, params=params)
     print(response)
-
-    
-song = runRequest("hi")
-print(song)
-addtrack(song)
