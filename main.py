753932d9cb191f34e6a03666d77ba74f5a5c637c
will <13015928+willwoodruff@users.noreply.github.com>
2019-11-19 14:13:36 +0000

    Adds playlist creation and a UI for the application

--- main.py
+++ main.py
@@ -1,11 +1,48 @@
 # Using Python 3 syntax as version 2 is being deprecated at the beginning of 2020
 import news_init
 import spotify_init
+from tkinter import *
+from tkmacosx import Button
+import webbrowser
 
-newsHeadlines = news_init.access_news_api()
 
-for row in newsHeadlines:
-    for i in row:
-        song = spotify_init.run_request(i)
-        if song is not None: 
-            spotify_init.add_track(song)
+def open_url(url):
+    webbrowser.open("https://open.spotify.com/playlist/" + url)
+
+
+def run_program():
+    lbl.configure(text="Your playlist is being generated...")
+    news_headlines = news_init.access_news_api()
+    playlist_id = spotify_init.create_playlist()
+    for row in news_headlines:
+        for keyword in row:
+            song = spotify_init.run_request(keyword)
+            if song is not None:
+                spotify_init.add_track(song, playlist_id)
+    lbl.configure(text="Your playlist has been generated! :)")
+    btn.configure(text="Click to Open", command=lambda: open_url(playlist_id))
+
+
+window = Tk()
+
+window.geometry("480x200")
+window.configure(background='#2B2722')
+##Creates the size of the window
+window.title("Spotify News Playlist Generator")
+##Adds a title to the Windows GUI for the window
+
+heading = Label(window, text=" Hello, welcome to the Spotify Playlist generator. \n", font=("Arial Bold", 20),
+                fg='#fff', bg='#2B2722')
+heading.place(relx=.5, rely=.5, anchor="center")
+lbl = Label(window,
+            text="We will get the latest news and match them to the Spotify library \n to generate you the songs of the day!\n",
+            fg='#fff', bg='#2B2722')
+
+heading.grid(column=0, row=0)
+lbl.grid(column=0, row=1)
+
+btn = Button(window, text="Click Me to Generate", bg="#68DA8F", fg="#fff", command=run_program)
+
+btn.grid(column=0, row=2)
+
+window.mainloop()
--- spotify_init.py
+++ spotify_init.py
@@ -2,6 +2,7 @@
 # Last Edited: 29/10/2019
 # Author(s): Will Woodruff
 # Collaborator(s): Craig Clephane
+import datetime
 
 import requests
 import json
@@ -9,7 +10,7 @@ import json
 headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer BQBz0dl-5IUhuX9GBiqSkY4PBw_feRjYox3s8uimXPwkUbrtifLmAy1yIyZGI3bBKxI32OZxjASxizegJdGcF38XLx9fJ_GvIGXoXjKqOsJyMv5mNV6kbTjxJvqb7x9eMA0GIg3BPXRjWDTuIXZr9_hTcUlAv6l1johypMxaPGI5D8vi1FmWhbqBU9cPr4C9RBbQheQpg_bzBSJI3Gy9GiZbaPz7miHRLHU56iLS-jhp7Nqw2RXygd1hxdaaWjYXOorqqbLWfm3TljYAvPjp_g4voP1aAA',
+    'Authorization': 'Bearer BQCFJ-e4SRAsThNu0mjSbQnwnNu_Uu63bEK3eK-yJXquRsQrqqKb-Ia-3HM4Iknq0470HADLvUcnzU-16JEO_Le1VPI0PwA9aHWTKRHHiwo-2UwZD7u6sBHYoc0wK8Ug4q4T85aUSkcwnl-T47hu52EpHQAk-aQ9aeNNOV6C59iEPyHNE-fInTNZRsOKb66PilSIq37869YJ2IhbBhpKscL4eDvPk5Ke0GB0m82c1hIgjrDRry1xIyphehhzntVCEkghxSsjxLYHOSF1nWdy_HLabnBGsA',
 }
 
 
@@ -39,17 +40,19 @@ def run_request(value):
 
 
 def create_playlist():
-    data = '{"name":"A New Playlist", "public":false}'
+    data = '{"name":"' + str(datetime.date.today()) + '", "public":false}'
     response = requests.post('https://api.spotify.com/v1/users/7wxlddrk3temavvzzao5qidyu/playlists', headers=headers,
                              data=data)
-    print(response)
+    json_data = json.loads(response.text)
+    playlist_id = json_data['external_urls']['spotify'].split('/')[-1]
+    return playlist_id
 
 
-def add_track(track):
+def add_track(track, playlist):
     params = (
         ('uris', 'spotify:track:' + track),
     )
 
-    response = requests.post('https://api.spotify.com/v1/playlists/1HsXvdPOcUvIsamfEMxuaM/tracks', headers=headers,
+    response = requests.post(f'https://api.spotify.com/v1/playlists/{playlist}/tracks', headers=headers,
                              params=params)
     print(response)
